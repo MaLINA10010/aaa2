18eca0c392a7c80a2b2fb006368acb82d5ad2cf4
Vasya <90628912+MaLINA10010@users.noreply.github.com>
2022-02-05 01:33:13 +0300

    Update JavaScript.sh

--- JavaScript.sh
+++ JavaScript.sh
@@ -2,16 +2,15 @@
 sudo apt install javascript-common -y
 sudo apt install nodejs -y
 sudo apt install npm -y
-sudo npm install -g\
-web3\
-vue\
-prettier\
-vue-router\
-http\
-express\
-react\
-react-bootstrap\
-react-scripts\
-create-react-app\
-react-router-dom\
-@vue/cli
+sudo npm install -g web3
+sudo npm install -g vue
+sudo npm install -g prettier
+sudo npm install -g vue-router
+sudo npm install -g http
+sudo npm install -g express
+sudo npm install -g react
+sudo npm install -g react-bootstrap
+sudo npm install -g react-scripts
+sudo npm install -g create-react-app
+sudo npm install -g react-router-dom
+sudo npm install -g @vue/cli
