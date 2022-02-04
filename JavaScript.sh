68ff0b19b9df2c4a07e57b8f77a2ade91fd0b341
Vasya <90628912+MaLINA10010@users.noreply.github.com>
2022-02-05 01:28:42 +0300

    Corrections

--- JavaScript.sh
+++ JavaScript.sh
@@ -3,5 +3,16 @@ sudo apt install -y\
 javascript-common\
 nodejs\
 npm
-sudo npm install -g web3
-prettier
+sudo npm install -g\
+web3\
+vue\
+prettier\
+vue-router\
+http\
+express\
+react\
+react-bootstrap\
+react-scripts\
+create-react-app\
+react-router-dom\
+@vue/cli
