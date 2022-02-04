76c4f194c19152c0df11e52e6bdfc15c6312dd65
Vasya <90628912+MaLINA10010@users.noreply.github.com>
2022-02-05 01:12:48 +0300

    Changes and additions to the Js Installer

---
+++ JavaScript.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo apt install -y\
+javascript-common\
+nodejs\
+npm
+sudo npm install -g web3
+prettier
--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@
 # sudo add-apt-repository ppa:graphics-drivers/ppa
 # sudo apt update -y
 # Установка софта
+sudo apt install curl -y
+sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add 
+sudo echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update -y
+sudo apt install yarn
 sudo apt install okular -y
 sudo apt install p7zip p7zip-rar p7zip-full -y
 sudo apt install dia -y
